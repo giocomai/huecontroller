bb383e7f56ac302541b1b1ca4376b30797396589
Giorgio Comai <g@giorgiocomai.eu>
2021-02-15 18:22:22 +0100

    add delete functions

---
+++ R/delete.R
@@ -0,0 +1,48 @@
+#' Delete light
+#'
+#' @param id If numeric, numeric id of light. If charachter, name of light. You can check id and names with `hue_get_lights_names()`
+#'
+#' @return Silently returns http response from the bridge, but mostly used for its side effects.
+#' @export
+#'
+#' @examples
+hue_delete_light <- function(id) {
+  check <- usethis::ui_yeah(x = paste0("Do you really want to delete the light ", id, "?"))
+  if (check == TRUE) {
+    response <- httr::DELETE(url = paste0("http://",
+                                          hue_settings()$hue_ip,
+                                          "/api/",
+                                          hue_settings()$hue_username,
+                                          "/lights/",
+                                          hue_output_id(id),
+                                          "/state"),
+                             body = body_json)
+    
+    invisible(response)
+  } else {
+    usethis::ui_info("Operation cancelled")  
+  }
+}
+
+#' Delete group
+#'
+#' @param id If numeric, numeric id of group. If charachter, name of group You can check id and names with `hue_get_groups_names()`
+#'
+#' @return Silently returns http response from the bridge, but mostly used for its side effects.
+#' @export
+#'
+#' @examples
+hue_delete_group <- function(id) {
+  check <- usethis::ui_yeah(x = paste0("Do you really want to delete the group ", id, "?"))
+  if (check == TRUE) {
+  response <- httr::DELETE(url = paste0("http://",
+                                        hue_settings()$hue_ip,
+                                        "/api/",
+                                        hue_settings()$hue_username,
+                                        "/groups/",
+                                        hue_output_group_id(id)))
+  invisible(response)
+  } else {
+    usethis::ui_info("Operation cancelled")  
+  }
+}
